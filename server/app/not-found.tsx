8c0acec6cb8c20405caa6d7c86f7b41f8544210f
CodeNaut1 <emilyyywellss@gmail.com>
2026-01-02 09:42:05 +0100

    87

--- server/app/not-found.tsx
+++ server/app/not-found.tsx
@@ -1,15 +1,17 @@
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-black">
       <div className="text-center">
         <h1 className="text-6xl font-bold mb-4 text-white">404</h1>
         <p className="text-xl text-gray-400 mb-8">Page Not Found</p>
-        <a
+        <Link
           href="/"
           className="text-blue-500 hover:text-blue-400 underline"
         >
           Return Home
-        </a>
+        </Link>
       </div>
     </div>
   );
